767146d225dbdf86d42123503bde9e6c7a75329d
Nagakumar2402 <nagakumar2402@gmail.com>
2025-02-16 15:33:27 +0530

    new Project

---
+++ script.js
@@ -0,0 +1,52 @@
+const right = document.querySelector(".right");
+const activity = document.querySelectorAll(".activity p");
+let timeframe = "daily";
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("./data.json", {
+      method: "GET",
+    });
+    const data = await response.json();
+    activity.forEach((singleActivity) => {
+      singleActivity.addEventListener("click", () => {
+        activity.forEach((act) => {
+          act.classList.remove("active");
+          singleActivity.classList.add("active");
+          timeframe = singleActivity.textContent.toLocaleLowerCase();
+          showData(data);
+        });
+      });
+    });
+    showData(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const showData = (data) => {
+  right.innerHTML = ""; // Clear existing content
+  data.forEach((singleData) => {
+    right.innerHTML += `
+      <div class="box ${singleData.title.toLowerCase()}">
+        <div class="imagcontainer">
+          <img src='./images/${singleData.title}.svg' alt='${
+      singleData.title
+    }' />
+        </div>
+        <div class="activityContainer">
+          <div class="act_title">
+            <p>${singleData.title}</p>
+            <img src="./images/icon-ellipsis.svg" alt="" />
+          </div>
+          <div class="time">
+            <h2>${singleData.timeframes[timeframe].current}hrs</h2>
+            <p>Previous - ${singleData.timeframes[timeframe].previous}hrs</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+};
+
+fetchData();
